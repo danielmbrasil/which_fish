da698e30e1e47592be13b37b9a7718e7de053112
Daniel M Brasil <danielmbrasil@protonmail.com>
2022-04-19 22:12:59 -0300

    add DGAN code

--- main.py
+++ main.py
@@ -1,29 +1,180 @@
-import cv2
+import tensorflow as tf
+import glob
+import imageio
+import matplotlib.pyplot as plt
+import numpy as np
 import os
+import cv2
+import pathlib
+from tensorflow.keras import layers
+import time
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Reshape
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Conv2DTranspose
 
-desired_size = 64
-filenames = os.listdir('images')
+from IPython import display
 
-for file in filenames:
-    im_pth = 'images/'+file
+batch_size = 32
+img_height = 64
+img_width = 64
+img_channels = 3
 
-    im = cv2.imread(im_pth)
-    old_size = im.shape[:2] # old_size is in (height, width) format
+dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    'images/', label_mode=None, image_size=(img_width, img_height),
+                   batch_size=batch_size
+)
 
-    ratio = float(desired_size)/max(old_size)
-    new_size = tuple([int(x*ratio) for x in old_size])
+dataset = dataset.map(lambda x: x / 255.0)
 
-    # new_size should be in (width, height) format
+discriminator = tf.keras.Sequential(
+    [
+        tf.keras.Input(shape=(img_width, img_height, img_channels)),
+        layers.Conv2D(64, kernel_size=4, strides=2, padding='same'),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv2D(128, kernel_size=4, strides=2, padding='same'),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv2D(128, kernel_size=4, strides=2, padding='same'),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Flatten(),
+        layers.Dropout(0.2),
+        layers.Dense(1, activation='sigmoid'),
+    ],
+    name="discriminator",
+)
 
-    im = cv2.resize(im, (new_size[1], new_size[0]))
+# discriminator.summary()
 
-    delta_w = desired_size - new_size[1]
-    delta_h = desired_size - new_size[0]
-    top, bottom = delta_h//2, delta_h-(delta_h//2)
-    left, right = delta_w//2, delta_w-(delta_w//2)
+latent_dim = 128
 
-    color = [255, 255, 255]
-    new_im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT,
-        value=color)
+generator = tf.keras.Sequential(
+    [
+        tf.keras.Input(shape=(latent_dim,)),
+        layers.Dense(8 * 8 * 128),
+        layers.Reshape((8, 8, 128)),
+        layers.Conv2DTranspose(128, kernel_size=4, strides=2, padding='same'),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv2DTranspose(256, kernel_size=4, strides=2, padding='same'),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv2DTranspose(512, kernel_size=4, strides=2, padding='same'),
+        layers.LeakyReLU(alpha=0.2),
+        layers.Conv2D(3, kernel_size=5, padding='same', activation='sigmoid'),
+    ],
+    name='generator',
+)
 
-    cv2.imwrite('images/'+file,new_im)
+# generator.summary()
+
+class GAN(tf.keras.Model):
+    def __init__(self, discriminator, generator, latent_dim):
+        super(GAN, self).__init__()
+        self.discriminator = discriminator
+        self.generator = generator
+        self.latent_dim = latent_dim
+
+    def compile(self, d_optimizer, g_optimizer, loss_fn):
+        super(GAN, self).compile()
+        self.d_optimizer = d_optimizer
+        self.g_optimizer = g_optimizer
+        self.loss_fn = loss_fn
+        self.d_loss_metric = tf.keras.metrics.Mean(name='d_loss')
+        self.g_loss_metric = tf.keras.metrics.Mean(name='g_loss')
+
+    @property
+    def metrics(self):
+        return [self.d_loss_metric, self.g_loss_metric]
+
+    def train_step(self, real_images):
+        # Sample random points in the latent space
+        batch_size = tf.shape(real_images)[0]
+        random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
+
+        # Decode them to fake images
+        generated_images = self.generator(random_latent_vectors)
+
+        # Combine them with real images
+        combined_images = tf.concat([generated_images, real_images], axis=0)
+
+        # Assemble labels discriminating real from fake images
+        labels = tf.concat(
+            [tf.ones((batch_size, 1)), tf.zeros((batch_size, 1))], axis=0
+        )
+
+        # Add random noise to the labels - important trick!
+        labels += 0.05 * tf.random.uniform(tf.shape(labels))
+
+        # Train the discriminator
+        with tf.GradientTape() as tape:
+            predictions = self.discriminator(combined_images)
+            d_loss = self.loss_fn(labels, predictions)
+        grads = tape.gradient(d_loss, self.discriminator.trainable_weights)
+        self.d_optimizer.apply_gradients(
+            zip(grads, self.discriminator.trainable_weights)
+        )
+
+        # Sample random points in the latent space
+        random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
+
+        # Assemble labels that say "all real images"
+        misleading_labels = tf.zeros((batch_size, 1))
+
+        # Train the generator (note that we should *not* update the weights
+        # of the discriminator)!
+        with tf.GradientTape() as tape:
+            predictions = self.discriminator(self.generator(random_latent_vectors))
+            g_loss = self.loss_fn(misleading_labels, predictions)
+        grads = tape.gradient(g_loss, self.generator.trainable_weights)
+        self.g_optimizer.apply_gradients(zip(grads, self.generator.trainable_weights))
+
+        # Update metrics
+        self.d_loss_metric.update_state(d_loss)
+        self.g_loss_metric.update_state(g_loss)
+        return {
+            'd_loss': self.d_loss_metric.result(),
+            'g_loss': self.g_loss_metric.result(),
+        }
+
+class GANMonitor(tf.keras.callbacks.Callback):
+    def __init__(self, num_img=3, latent_dim=128):
+        self.num_img = num_img
+        self.latent_dim = latent_dim
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.save_checkpoint(epoch)
+        random_latent_vectors = tf.random.normal(shape=(self.num_img, self.latent_dim))
+        generated_images = self.model.generator(random_latent_vectors)
+        generated_images *= 255
+        generated_images.numpy()
+        for i in range(self.num_img):
+            img = tf.keras.preprocessing.image.array_to_img(generated_images[i])
+            img.save('results/generated_img_%03d_%d.png' % (epoch, i))
+
+    # save checkpoint every 5 epochs
+    def save_checkpoint(self, epoch):
+        if (epoch % 5) == 0:
+            checkpoint_dir = './training_checkpoints'
+            checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+            checkpoint = tf.train.Checkpoint(generator_optimizer=self.model.g_optimizer,
+                                    discriminator_optimizer=self.model.d_optimizer,
+                                    generator=self.model.generator,
+                                    discriminator=self.model.discriminator)
+            checkpoint.save(file_prefix = checkpoint_prefix)
+
+epochs = 100
+
+gan = GAN(discriminator=discriminator, generator=generator, latent_dim=latent_dim)
+gan.compile(
+    d_optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001),
+    g_optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001),
+    loss_fn = tf.keras.losses.BinaryCrossentropy(),
+)
+
+gan.fit(
+    dataset, epochs=epochs, callbacks=[GANMonitor(num_img=10, latent_dim=latent_dim)]
+)
