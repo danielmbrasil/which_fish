18086e4510405d46bc0ac0d7e6b873af944f3b2d
Daniel M Brasil <danielmbrasil@protonmail.com>
2022-04-19 22:00:34 -0300

    turn images into 64x64

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
+import cv2
 import os
-import random
 
+desired_size = 64
 filenames = os.listdir('images')
-random.shuffle(filenames)
-filenames = filenames[10000:]
 
 for file in filenames:
-    os.remove('images/'+file)
+    im_pth = 'images/'+file
+
+    im = cv2.imread(im_pth)
+    old_size = im.shape[:2] # old_size is in (height, width) format
+
+    ratio = float(desired_size)/max(old_size)
+    new_size = tuple([int(x*ratio) for x in old_size])
+
+    # new_size should be in (width, height) format
+
+    im = cv2.resize(im, (new_size[1], new_size[0]))
+
+    delta_w = desired_size - new_size[1]
+    delta_h = desired_size - new_size[0]
+    top, bottom = delta_h//2, delta_h-(delta_h//2)
+    left, right = delta_w//2, delta_w-(delta_w//2)
+
+    color = [255, 255, 255]
+    new_im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT,
+        value=color)
+
+    cv2.imwrite('images/'+file,new_im)
